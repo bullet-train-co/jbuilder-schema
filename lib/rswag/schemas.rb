7d5a64a4922a691a6e141ab21cfe387df2b99f18
Yuri Sidorov <yurisidorov@evrone.com>
2022-07-15 22:46:18 +0200

    Initial commit

---
+++ lib/rswag/schemas.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "schemas/version"
+
+module Rswag
+  module Schemas
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
