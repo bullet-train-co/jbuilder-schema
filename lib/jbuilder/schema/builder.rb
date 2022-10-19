3cd144d72eb36faa78d820d03377a66c9a4d4520
Kasper Timm Hansen <kaspth@gmail.com>
2022-10-19 23:51:41 +0200

    Inline the remaining state the builder isn't really using

--- lib/jbuilder/schema/builder.rb
+++ lib/jbuilder/schema/builder.rb
@@ -7,9 +7,8 @@ module JbuilderSchema
   # Class that builds schema object from path
   class Builder
     def initialize(path, paths: ["app/views"], **options)
-      @path = path
-      @paths = paths
-      @template = Renderer.new(**options).render(_find_template)
+      source = find_template_source(paths, path)
+      @template = Renderer.new(**options).render(source)
     end
 
     def schema!
@@ -18,12 +17,12 @@ module JbuilderSchema
 
     private
 
-    def _find_template
-      *prefixes, controller, action = @path.split("/")
+    def find_template_source(paths, path)
+      *prefixes, controller, action = path.split("/")
       prefix = prefixes.join("/")
       partial = true if action.delete_prefix! "_"
 
-      @paths.each do |path|
+      paths.each do |path|
         found = Resolver.new("#{path}/#{prefix}").find(action, controller, partial)
         return found if found
       end
