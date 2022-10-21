51e1469951f44e41e84d86cdef7e517978bde3ad
Kasper Timm Hansen <kaspth@gmail.com>
2022-10-21 19:04:23 +0200

    Test our monkey patch and use keyword arguments to pluck out the schema

--- lib/jbuilder/jbuilder.rb
+++ lib/jbuilder/jbuilder.rb
@@ -1,23 +1,9 @@
 # frozen_string_literal: true
 
-require "jbuilder"
-
-# Patches for Jbuilder to make it ignore schema metadata
-class Jbuilder
-  alias_method :original_method_missing, :method_missing
-
-  def method_missing(*args, &block)
-    args = _extract_schema_meta!(*args)
-    original_method_missing(*args, &block)
-  end
-
-  def respond_to_missing?(method_name, include_private = false)
-    super
-  end
-
-  private
-
-  def _extract_schema_meta!(*args)
-    args.delete_if { |a| a.is_a?(::Hash) && a.key?(:schema) }
+module JbuilderIgnoreSchemaMeta
+  def method_missing(*args, schema: nil, **options, &block) # standard:disable Style/MissingRespondToMissing
+    super(*args, **options, &block)
   end
 end
+
+Jbuilder.prepend JbuilderIgnoreSchemaMeta
