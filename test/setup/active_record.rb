b8e1cbfa78da32f3d1c71c3df880d927be4b5e29
Kasper Timm Hansen <kaspth@gmail.com>
2022-10-22 17:08:29 +0200

    Add timestamps to users just to be consistent

--- test/setup/active_record.rb
+++ test/setup/active_record.rb
@@ -6,6 +6,7 @@ ActiveRecord::Schema.define do
   create_table :users, force: true do |t|
     t.string :name, null: false
     t.string :email, null: false
+    t.timestamps
   end
 
   create_table :articles, force: true do |t|
