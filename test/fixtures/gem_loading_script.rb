69b4ae83af0fca7c692659aa71f4a7c9f4438f0f
Kasper Timm Hansen <kaspth@gmail.com>
2022-10-22 19:11:57 +0200

    Fix loading script; needed require bigdecimal & status since partial uses it

--- test/fixtures/gem_loading_script.rb
+++ test/fixtures/gem_loading_script.rb
@@ -1,5 +1,7 @@
 require "bundler/inline"
 
+require "bigdecimal"
+
 gemfile do
   source "https://rubygems.org"
 
@@ -7,6 +9,6 @@ gemfile do
   gem "jbuilder-schema", path: "../jbuilder-schema"
 end
 
-article = Struct.new(:id, :title, :body, :created_at, :updated_at).new(1, "yo", "sup", DateTime.now, DateTime.now)
+article = Struct.new(:id, :status, :title, :body, :created_at, :updated_at).new(1, "pending", "yo", "sup", DateTime.now, DateTime.now)
 
 puts Jbuilder::Schema.yaml "api/v1/articles/_article", paths: ["test/fixtures"], locals: { article: article }
