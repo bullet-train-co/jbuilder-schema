c77bd9dfdb49e037a03b9af55c3d383ae2f374bd
Yuri Sidorov <403994+newstler@users.noreply.github.com>
2023-10-16 18:55:25 +0200

    Replace type: null with nullable: true

--- lib/jbuilder/schema/template.rb
+++ lib/jbuilder/schema/template.rb
@@ -173,10 +173,7 @@ class Jbuilder::Schema
     end
 
     def _nullify_non_required_types(attributes, required)
-      attributes.transform_values! {
-        _1[:type] = [_1[:type], "null"] unless required.include?(attributes.key(_1))
-        _1
-      }
+      attributes.transform_values! { _1[:nullable] = true unless required.include?(attributes.key(_1)); _1 }
     end
 
     def _set_description(key, value)
