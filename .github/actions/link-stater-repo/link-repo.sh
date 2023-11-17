38521d83e6a672f4d6571d4a2082025c2bf4361f
Jeremy Green <jeremy@octolabs.com>
2023-11-17 14:56:34 -0600

    more consolidation

--- .github/actions/link-stater-repo/link-repo.sh
+++ .github/actions/link-stater-repo/link-repo.sh
@@ -1,25 +1,9 @@
 #!/usr/bin/env bash
 
-# Default to the main branch if we don't find a matching branch on the starter repository.
-STARTER_REPO_BRANCH="main"
+STARTER_PATH=$WORKSPACE/$STARTER_DIR
 
-# Look for a matching branch on the starter repository when running tests in CI
-CI_BRANCH=$TARGET_BRANCH
-if [[ -v CI_BRANCH ]]
-then
-  BRANCH_RESPONSE=$(curl https://api.github.com/repos/bullet-train-co/bullet_train/branches/$CI_BRANCH)
-
-  echo "Branch response ===================="
-  echo $BRANCH_RESPONSE
-
-  # If the branch is missing in the repo the response will not contain the branch name
-  if echo $BRANCH_RESPONSE | grep "$TARGET_BRANCH"; then
-    STARTER_REPO_BRANCH=$CI_BRANCH
-  fi
-fi
-
-echo "Cloning from ${STARTER_REPO_BRANCH}..."
-git clone -b $STARTER_REPO_BRANCH --depth 1 https://github.com/bullet-train-co/bullet_train.git .
+echo "STARTER_PATH = ${STARTER_PATH}"
+cd $STARTER_PATH
 
 echo "gem \"jbuilder-schema\", path: \"../../\"" >> Gemfile
 
